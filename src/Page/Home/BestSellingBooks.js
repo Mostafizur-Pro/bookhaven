7d9c6aae6311a86db078d86fed81bfabadc1b68f
Mostafizur-pro <mostafizur0195@gmail.com>
2023-04-05 22:24:21 +0600

    best selling books add

---
+++ src/Page/Home/BestSellingBooks.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BestSellingBooks = () => {
+    return (
+        <div className='my-5'>
+            <div>
+                <p>People's Choise</p>
+                <h2 className='text-2xl'>Best Selling Books</h2>
+            </div>
+            <hr/>
+        </div>
+    );
+};
+
+export default BestSellingBooks;
--- src/Page/Home/HomePage.js
+++ src/Page/Home/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BannerPage from './BannerPage';
 import BannerPage2 from './BannerPage2';
+import BestSellingBooks from './BestSellingBooks';
 
 const HomePage = () => {
     return (
         <div>
             <BannerPage/>
             <BannerPage2/>
+            <BestSellingBooks/>
         </div>
     );
 };
