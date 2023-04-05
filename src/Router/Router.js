50babdd05a3f4744c524390df1beb7a0864089fc
Mostafizur-pro <mostafizur0195@gmail.com>
2023-04-05 21:41:10 +0600

    navber add

---
+++ src/Router/Router.js
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import HomePage from "../Page/Home/HomePage";
+
+const router = createBrowserRouter([
+    {
+        path: "*",
+        element: <h1>Error Page</h1>,
+      },
+      {
+        path: '/',
+        element: <Main/>,
+        children: [
+            {
+                path: '/',
+                element: <HomePage/>
+            }
+        ]
+      }
+
+])
+
+export default router;
