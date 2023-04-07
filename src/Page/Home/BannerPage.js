5c63f771236f14e4810338864e36501952bde6f5
Mostafizur-pro <mostafizur0195@gmail.com>
2023-04-07 11:42:50 +0600

    banner page edit

--- src/Page/Home/BannerPage.js
+++ src/Page/Home/BannerPage.js
@@ -12,11 +12,9 @@ const BannerPage = () => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-right text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-4xl font-bold">Welcome to Open Library</h1>
+          <h1 className="mb-5 text-4xl font-bold">LOTS OF EBOOKS</h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Welcome to your frinedly neighborhood library. We have more than 15,000 free ebooks waiting to be discovered.
           </p>
           <button className="btn btn-primary">Product List</button>
         </div>
--- src/Page/Home/BannerPage2.js
+++ src/Page/Home/BannerPage2.js
@@ -4,42 +4,59 @@ import image2 from '../../asset/homepage/Icon/bgparallax-02.jpg'
 import image3 from '../../asset/homepage/Icon/bgparallax-03.jpg'
 
 const BannerPage2 = () => {
-    const [items, setItems] = useState([])
-    useEffect(() => {
-        fetch(
-          "BannerPage2.json"
-        )
-          .then((res) => res.json())
-          .then((data) => {
-            // console.log(data);
-            setItems(data);
-          });
-      }, []);
+   
 
     return (
-        <div className='grid grid-cols-3 gap-5 mt-5'>
-{items.map(item=><>
-    <section className="hero h-48  " style={{ 
-            backgroundImage: `url(${image2})`, }}>
+        <div className='grid  grid-cols-3 gap-5 mt-5'>
+
+    <section className="hero h-full w-full" style={{ 
+            backgroundImage: `url(${image1})`, }}>
   <div className="hero-overlay bg-opacity-70"></div>
-  <div className='border-2 border-white  '>
+
 
   <div className="hero-content text-right text-neutral-content">
     <div className="max-w-md">
-      <h1 className=" text-2xl font-bold p-10">{item?.text}
+      <h1 className=" text-2xl font-bold p-10">Books we have
       <br/>
-      {item?.Qty}</h1>
+      1,430</h1>
       
       
     </div>
   </div>
+
+</section>
+<section className="hero h-full w-full" style={{ 
+            backgroundImage: `url(${image2})`, }}>
+  <div className="hero-overlay bg-opacity-70"></div>
+
+
+  <div className="hero-content text-right text-neutral-content">
+    <div className="max-w-md">
+      <h1 className=" text-2xl font-bold p-10">Total Members
+      <br/>
+      610</h1>
+      
+      
+    </div>
+  </div>
+
+</section>
+<section className="hero h-full w-full"style={{ 
+            backgroundImage: `url(${image3})`, }}>
+  <div className="hero-overlay bg-opacity-70"></div>
+
+
+  <div className="hero-content text-right text-neutral-content">
+    <div className="max-w-md">
+      <h1 className=" text-2xl font-bold p-10">Happy Users<br/>
+      559</h1>
+      
+      
+    </div>
+
   </div>
 </section>
-
-</>)}
-             
-            
-           
+     
         </div>
     );
 };
