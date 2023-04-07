47ec563b06978954464367fec3ec18af41b8ecd7
Mostafizur-pro <mostafizur0195@gmail.com>
2023-04-07 12:34:10 +0600

    home page complete

--- src/Page/Home/HomePage.js
+++ src/Page/Home/HomePage.js
@@ -2,6 +2,10 @@ import React from 'react';
 import BannerPage from './BannerPage';
 import BannerPage2 from './BannerPage2';
 import BestSellingBooks from './BestSellingBooks';
+import NewReleaseBooks from './NewReleaseBooks';
+import MostPopularAuthors from './MostPopularAuthors';
+import OpenDiscount from './OpenDiscount';
+import Comments from './Comments';
 
 const HomePage = () => {
     return (
@@ -9,6 +13,10 @@ const HomePage = () => {
             <BannerPage/>
             <BannerPage2/>
             <BestSellingBooks/>
+            <OpenDiscount/>
+            <NewReleaseBooks/>
+            <Comments/>
+            <MostPopularAuthors/>
         </div>
     );
 };
